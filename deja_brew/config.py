fc536d6d01fc146f6f3bb9ba16dbd7d3fe10cdd1
Nick Whyte <nick@nickwhyte.com>
2020-02-23 22:17:40 +1100

    .

--- deja_brew/config.py
+++ deja_brew/config.py
@@ -22,3 +22,4 @@ class ProductionConfig(Config):
         super(ProductionConfig, self).__init__()
 
         self.SECRET_KEY = os.environ['SECRET_KEY']
+        self.SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
