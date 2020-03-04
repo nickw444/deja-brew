ab2761497e4b7fc69b7ba23bde270adb1478c0aa
Nick Whyte <nick@nickwhyte.com>
2020-03-04 19:15:23 +1100

    support specific version deploy

--- production/bin/deploy.sh
+++ production/bin/deploy.sh
@@ -4,8 +4,58 @@ set -eu
 REPO_DIR="$(git rev-parse --show-toplevel)"
 VERSION="$(git rev-parse --short HEAD)"
 
+usage() {
+  local prog
+  prog=$(basename "$0")
+  cat <<EOF
+Usage: ${prog} [ OPTIONS... ]
+
+  Runs the main 100-warm-tunas pipeline
+
+  Options:
+    --version "${VERSION}"           Version of the container to deploy. Default to the
+                                  current commit sha (${VERSION})
+
+  Usage:
+    ${prog}
+      Deploy the version at HEAD
+
+    ${prog} --version "${VERSION}"
+      Deploy a specific tagged version of the application container
+
+EOF
+  exit
+}
+
+
 main() {
   cd "${REPO_DIR}/production"
+  local version="${VERSION}"
+
+  while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+      -h|--help)
+        usage
+        ;;
+      --version)
+        shift;
+        version="$1"
+        ;;
+      -*|--*|*)
+        echo "unknown option '$1'"
+        exit 1
+        ;;
+    esac
+    shift
+  done
+
+  echo "Do you wish to deploy version: "
+  read -p "Continue with deployment of: ${VERSION} [y/N]" -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      echo "Exiting!"
+      exit 1;
+  fi;
 
   local tmp_dest;
   tmp_dest=$(mktemp)
