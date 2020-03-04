d84f704f49a13065c59ffdab34be42e846c79e5d
Nick Whyte <nick@nickwhyte.com>
2020-03-04 22:30:32 +1100

    Disable double tap to zoom on iOS

--- frontend/src/skeleton/skeleton.tsx
+++ frontend/src/skeleton/skeleton.tsx
@@ -14,6 +14,7 @@ export const Skeleton = React.memo(({
   AppTabs: React.ComponentType,
   history: History,
 }) => (
+    <div className={styles.skeleton}>
     <Router history={history}>
       <div className={styles.headerContainer}>
         <Header/>
@@ -23,4 +24,5 @@ export const Skeleton = React.memo(({
       </main>
       <AppTabs/>
     </Router>
+    </div>
 ));
