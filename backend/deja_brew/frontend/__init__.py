b1b4e64cff435ad8637728be363cbaaa2f8a3daf
Nick Whyte <nick@nickwhyte.com>
2020-03-04 22:05:31 +1100

    Add favicons to backend

--- backend/deja_brew/frontend/__init__.py
+++ backend/deja_brew/frontend/__init__.py
@@ -1,7 +1,7 @@
 import json
 from os import path
 
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, jsonify, send_from_directory, url_for
 from flask_login import current_user
 
 from deja_brew.frontend.asset_manifest_supplier import AssetManifestSupplier
@@ -14,6 +14,7 @@ frontend_bp = Blueprint(
     static_folder='build/static',
     static_url_path='/static/'
 )
+
 
 @frontend_bp.route('/')
 @frontend_bp.route('/login')
@@ -33,3 +34,40 @@ def frontend(_: str = None):
         json_bootstrap=json.dumps(bootstrap),
         manifest=manifest,
     )
+
+
+@frontend_bp.route('/manifest.json')
+def web_app_manifest():
+    return jsonify({
+        'short_name': 'Deja Brew',
+        'name': 'Deja Brew',
+        'icons': [
+            {
+                "src": url_for(".icons_static", filename="avicon.ico"),
+                "sizes": "64x64 32x32 24x24 16x16",
+                "type": "image/x-icon"
+            },
+            {
+                "src": url_for(".icons_static", filename="ndroid-chrome-192x192.png"),
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": url_for(".icons_static", filename="ndroid-chrome-384x384.png"),
+                "sizes": "384x384",
+                "type": "image/png"
+            },
+        ],
+        "start_url": ".",
+        "display": "standalone",
+        "theme_color": "#333333",
+        "background_color": "#ffffff"
+    })
+
+
+icons_static_dir = path.join(path.dirname(__file__), 'icons')
+
+
+@frontend_bp.route('/static/icons/<path:filename>')
+def icons_static(filename: str):
+    return send_from_directory(icons_static_dir, filename)
